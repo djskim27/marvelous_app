2bbda8455c20f659eba98936b38ac276ca824ff5
djskim27 <djs.kim27@gmail.com>
2017-09-12 13:13:23 -0400

    added navbar

--- client/src/components/MainNavBar.jsx
+++ client/src/components/MainNavBar.jsx
@@ -1,12 +1,38 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+const Nav = styled.div`
+width: 95%;
+display: flex;
+justify-content: space-between;
+align-items: center;
+padding: 0 2.5%;
+background-color: rgba(228, 16, 12, 1);
+box-shadow: 0px 1px 6px black;
+a {
+  text-decoration: none;
+  margin: 0 5px;
+  &:visited {
+    color: white;
+  }
+}
+`;
+
+
 export default class MainNavBar extends Component {
   render() {
     return (
-      <div>
-          <Link to='/'>Home</Link>
+      <Nav>
+          
+          <h2><Link to='/'>MARVELous</Link></h2>
+
+          <div>
+            <Link to='#'>Sign Up</Link>
+            <Link to='#'>Sign In</Link>
+          </div>
         
-      </div>
+      </Nav>
     )
   }
 }
